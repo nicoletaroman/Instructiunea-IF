bb4d19bdc8c69279557cc846d397f8dbbeb8b9d5
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-10-13 19:34:59 +0300

    Add files via upload

---
+++ problema 2 IF.py
@@ -0,0 +1,10 @@
+n1=int(input("dati nr"))
+n2=int(input("dati nr"))
+n3=int(input("dati nr"))
+n1<3200
+n2<3200
+n3<3200
+if n1<n2+n3 and n2<n1+n3 and n3<n1+n2:
+    print("da")
+else:
+    print("nu")
