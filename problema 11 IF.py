c8eaa587188a5419f7d785dd9823a1cd3e9aa2ca
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-10-14 20:30:38 +0300

    Add files via upload

---
+++ problema 11 IF.py
@@ -0,0 +1,40 @@
+a=int(input("dati un nr"))
+b=int(input("dati un nr"))
+c=int(input("dati un nr"))
+if a%2==0 and b%2==0 and c%2==0:
+    print(a,"par")
+    print(b,"par")
+    print(c,"par")
+if a%2==0 and b%2!=0 and c%2==0:
+    print(a,"par")
+    print(b,"impar")
+    print(c,"par")
+if a%2==0 and b%2==0 and c%2!=0:
+    print(a,"par")
+    print(b,"par")
+    print(c,"impar")
+if a%2!=0 and b%2==0 and c%2==0:
+    print(a,"impar")
+    print(b,"par")
+    print(c,"par")
+if a%2!=0 and b%2!=0 and c%2!=0:
+    print(a,"impar")
+    print(b,"impar")
+    print(c,"impar")
+if a%2==0 and b%2!=0 and c%2!=0:
+    print(a,"par")
+    print(b,"impar")
+    print(c,"impar")
+if a%2!=0 and b%2!=0 and c%2==0:
+    print(a,"impar")
+    print(b,"impar")
+    print(c,"par")
+if a%2!=0 and b%2==0 and c%2!=0:
+    print(a,"impar")
+    print(b,"par")
+    print(c,"impar")
+
+
+
+
+
