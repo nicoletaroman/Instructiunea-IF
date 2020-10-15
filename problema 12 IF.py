710751b51ae969f953228a87ec66f6a9741578ee
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-10-15 08:59:38 +0300

    Add files via upload

---
+++ problema 12 IF.py
@@ -0,0 +1,13 @@
+x=int(input("dati un nr"))
+if x%5==0:
+    print("deloc")
+if x%5==1:
+    print("un pic")
+if x%5==2:
+    print("mult")
+if x%5==3:
+    print("cu pasiune")
+if x%5==4:
+    print("la nebunie")
+
+
