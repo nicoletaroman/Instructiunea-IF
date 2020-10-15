bfc7bad8649f4a8fc459fa7d97842fdde1474cbc
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-10-15 13:50:27 +0300

    Add files via upload

---
+++ problema 15 IF.py
@@ -0,0 +1,13 @@
+x=int(input("dati nr elevului"))
+if x>0 and x<=25:
+    print("A")
+if x>25 and x<=50:
+    print("B")
+if x>50 and x<=75:
+    print("C")
+if x>75 and x<=100:
+    print("D")
+if x>100 and x<=125:
+    print("E")
+if x>125:
+    print("eroare")
