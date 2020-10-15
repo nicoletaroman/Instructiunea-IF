a2e7c01a114d838446f165b3618455d7a58916bd
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-10-15 09:04:40 +0300

    Add files via upload

---
+++ problema 13 IF.py
@@ -0,0 +1,11 @@
+x=int(input("dati nr concurentului"))
+if x>100:
+    print("se dau premii primilor 100")
+if x%4==0:
+    print:("neagra")
+if x%4==1:
+    print("alba")
+if x%4==2:
+    print("rosie")
+if x%4==3:
+    print("albastra")
